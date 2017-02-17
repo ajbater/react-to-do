53bf238105e1b95a9ae9e2702c9f72b1f0cf9b58
Amanda Bater <amandabater@Amandas-MacBook-Pro.local>
2017-02-17 15:59:21 +0000

    set up pure render mixin addon

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin'
 import ToDoItem from './ToDoItem';
 
 export default class ToDoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+  }
   render() {
     <section className="main">
       <ul className="todo-list">
