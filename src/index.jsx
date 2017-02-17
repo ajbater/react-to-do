698da65d1e12d8083d74f6778eff5f0206e274ff
Amanda Bater <amandabater@Amandas-MacBook-Pro.local>
2017-02-17 15:17:46 +0000

    attaches ToDoApp to index.html

---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {List, Map} from 'immutable';
+
+import ToDoApp from './components/ToDoApp';
+
+const todos = List.of(
+  Map({id: 1, text: 'React', status: 'active', editing: 'false'}),
+  Map({id: 1, text: 'Redux', status: 'active', editing: 'false'}),
+  Map({id: 1, text: 'Immutable', status: 'active', editing: 'false'})
+);
+
+ReactDOM.render(
+  <ToDoApp todos={todos} />,
+  document.getElementById("app")
+);
