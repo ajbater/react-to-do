465f125168af324f9017f54234cd55e597cdbe0c
Amanda Bater <amandabater@Amandas-MacBook-Pro.local>
2017-02-17 15:42:48 +0000

    extracts some of ToDoApp into ToDoList component

--- src/components/ToDoApp.jsx
+++ src/components/ToDoApp.jsx
@@ -1,28 +1,11 @@
 import React from 'react';
+import ToDoList from './ToDoList'
 
 export default class ToDoApp extends React.Component {
-  getItems() {
-    return this.props.todos || [];
-  }
   render () {
     return <div>
       <section className="todoapp">
-        <section className="main">
-          <ul className="todo-list">
-            {this.getItems().map(item =>
-              <li className="active" key={item.get('text')}>
-                <div className="view">
-                  <input type="checkbox"
-                    className="toggle" />
-                  <label htmlFor="todo">
-                    {item.get('text')}
-                  </label>
-                  <button className="destroy"></button>
-                </div>
-              </li>
-            )}
-          </ul>
-        </section>
+        <ToDoList todos={this.props.todos} />
       </section>
     </div>
   }
---
+++ src/components/ToDoList.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ToDoItem from './ToDoItem';
+
+export default class ToDoList extends React.Component {
+  render () {
+    <section className="main">
+      <ul className="todo-list">
+        {this.props.todos.map(item=>
+          <ToDoItem key={item.get('text')}
+                    key="item.get('text')" />
+        )}
+      </ul>
+    </section>
+  }
+}
