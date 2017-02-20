317e0d4bf03336c502c20fc9a029b90f433c664a
Amanda Bater <amandabater@Amandas-MacBook-Pro.local>
2017-02-20 17:44:53 +0000

    adds footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React 'react';
+
+export default React.createClass({
+  render: function () {
+    return <footer className="info">
+      <p>Double-click to edit a todo</p>
+    </footer>
+  }
+});
