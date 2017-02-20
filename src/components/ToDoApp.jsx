c3eb06cdfb11b6b705984762a444c17183960b17
Amanda Bater <amandabater@Amandas-MacBook-Pro.local>
2017-02-20 17:32:29 +0000

    ToDoApp renders the ToDoHeader

--- src/components/ToDoApp.jsx
+++ src/components/ToDoApp.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ToDoList from './ToDoList'
+import ToDoHeader from './ToDoHeader'
 
 export default class ToDoApp extends React.Component {
   render() {
     return <div>
       <section className="todoapp">
+        <ToDoHeader />
         <ToDoList todos={this.props.todos} filter={this.props.filter} />
       </section>
     </div>
